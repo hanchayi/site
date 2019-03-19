beb513fcb61711873287a3e26d37a76babbf79f9
zhangyi <zhangyi@corp-ci.com>
2019-03-19 21:14:53 +0800

    初步架子搭起来

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config = require('./config')
 const path = require('path')
 const app = express()
 
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 
 const website = {
@@ -10,38 +11,98 @@ const website = {
   logo: '',       // 公司logo
   menus: [
     {
+      label: '首页',
+      path: '/'
+    },
+    {
       label: '公司信息',
-      route: '/company'
+      path: '/company'
     },
     {
       label: '企业文化',
-      route: '/culture'
+      path: '/culture'
     },
     {
       label: '业务体系',
-      route: '/system'
+      path: '/system'
     },
     {
       label: '投资者关系',
-      route: '/investor'
+      path: '/investor'
     }
   ]
 } 
 
 // 首页
-app.get('/', function (req, res) {
+app.get('/', function ({
+  path
+}, res) {
+  const news = [
+    {
+      title: '新科技',
+      desc: '构筑智慧未来'
+    },
+    {
+      title: '新文化',
+      desc: '丰富精神生活'
+    },
+    {
+      title: '社会责任',
+      desc: '履行历史担当'
+    }
+  ]
+
   const params = {
+    path,
+    news,
     website
   }
   res.render('page-index/index.jade', params)
 })
 
 // 公司
-app.get('/company', function (req, res) {
+app.get('/company', function ({
+  path
+}, res) {
   const params = {
+    path,
     website
   }
   res.render('page-company/index.jade', params)
+})
+
+// 企业文化
+app.get('/culture', function ({
+  path
+}, res) {
+  const params = {
+    path,
+    website
+  }
+  res.render('page-culture/index.jade', params)
+})
+
+
+// 业务体系
+app.get('/system', function ({
+  path
+}, res) {
+  const params = {
+    path,
+    website
+  }
+  res.render('page-system/index.jade', params)
+})
+
+// 投资者关系
+app.get('/investor', function ({
+  path
+}, res) {
+  const params = {
+    path,
+    website
+  }
+  res.render('page-investor/index.jade', params)
 })
 
 app.listen(config.port)
